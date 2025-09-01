ff3fd03635f522216d9fd0db3fd56f65fcc8a1d2
Alex <alex_p97@live.com>
2025-09-01 23:08:05 +0100

    Planned out basic terrian generation logic

--- scripts/terrain.js
+++ scripts/terrain.js
@@ -18,5 +18,29 @@ export class TerrainGenerator {
         };
         // How far ahead to generate terrain
         this.maxDrawDistance = canvas.height * 1.5; 
+        this.safeZone = 3
     }
+
+    // Create the initial terrain on game start
+    generateInitialTerrain() {
+
+        // Use safe zone value to create initial grass rows
+        for (let i = 0; i < this.safeZone; i++) {
+            this.generateTerrainRow('grass');
+        }
+
+        // Generate the remainder of rows using draw distance
+        for (let i = 0; i < this.maxDrawDistance; i++) {
+            this.generateRandomTerrainRow();
+        }
+    }
+
+    /* generateRandomTerrainRow() {
+        This function generates a random terrain and then calls generateTerrainRow(type)
+    } */
+
+    /* generateTerrainRow(type) {
+        This function takes a defined terrain type and generates a row of that type
+    } */
+
 }
