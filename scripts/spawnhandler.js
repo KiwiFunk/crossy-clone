943de0fdba1f1763d412352fe123121f51239242
Alex <alex_p97@live.com>
2025-09-24 22:16:22 +0100

    Planned out start of asset spawner

--- scripts/spawnhandler.js
+++ scripts/spawnhandler.js
@@ -30,24 +30,35 @@ export class SpawnManager {
     }
 
     spawnAssets() {
-        // Configs for this asset
-        const numLogs = Math.floor(Math.random() * 3) + 1; // 1-3 logs
-        const direction = Math.random() > 0.5 ? 'right' : 'left';
-        const speedValue = Math.random() * 0.03 + 0.02;
-        const signedSpeed = direction === 'right' ? speedValue : -speedValue;
 
-        // Row dimensions
-        const rowHalfWidth = (CONFIG.ROW_WIDTH_IN_TILES * CONFIG.TILE_SIZE) / 2; // 10 meters
-        const spawnDistance = 5; // 5 meters outside the view
+        // 1. Get the row dimensions
+        const rowHalfWidth = (CONFIG.ROW_WIDTH_IN_TILES * CONFIG.TILE_SIZE) / 2;
 
-        // Set spawn side based on direction
-        const startX = direction === 'right' ? -rowHalfWidth - spawnDistance : rowHalfWidth + spawnDistance;
+        // 2. If !Static, determine direction and speed
+        if(this.options.isMoving) {
+            const direction = Math.random() > 0.5 ? 'right' : 'left';
+            const baseSpeed = this.getAppropriateSpeed();
+            const speed = direction === 'right' ? baseSpeed : -baseSpeed;
+        }
 
-        // Keep track of assets to avoid overlaps if there are multiple
-        const occupiedZones = [];
+        // Moving objects spawn offscreen
+        let startX = 0;
+        if (this.options.isMoving) {
+            const spawnDistance = 5; // 5 meters outside view
+            startX = direction === 'right' 
+                ? -rowHalfWidth - spawnDistance 
+                : rowHalfWidth + spawnDistance;
+        }
+
+        // Plan object positions to avoid overlap
+        const plannedPositions = [];
+        const spawnedEntities = [];
+
+        // 3. Create asset plans to avoid overlap
+        const plans = this.createAssetPlans();
 
         // Loop through using count parameter
-        for (let i = 0; i < this.count; i++) {
+        for (let i = 0; i < this.plans.length; i++) {
             // Clone the mesh and add it to an array of objects to add
             // Add the XPos to the occupiedZones width, using the model width to calculate the range
             // If the randomized x location is in this array, try again
@@ -58,4 +69,12 @@ export class SpawnManager {
         // Push the collection of assets to the scene
         }
     }
+
+    createAssetPlans() [
+        // Plan out our assets for this row
+        // If we are spawnming multiple, we need to use object widths to avoid overlap
+        // Return an array of planned X positions
+        // Assets comprised of multiple parts need their class to return some kind of flag to allow this to be handled. We can return a 2D array with the width of each part in the sub-array
+
+    ]
 }
